d17b8295c7411a81fd332fab2ab84c223b9fe845
dueytree <dueytree.00@gmail.com>
2021-03-18 00:36:24 +0900

    creating view using APIView.

--- instagram/urls.py
+++ instagram/urls.py
@@ -8,6 +8,6 @@ router.register('post', views.PostViewSet)   # 2개 URL을 만들어줍니다.
 # router.urls   # urls pattern list
 
 urlpatterns = [
-    path('public/', views.PublicPostListAPIView.as_view()),
+    path('public/', views.public_post_list),
     path('', include(router.urls)),
 ]
